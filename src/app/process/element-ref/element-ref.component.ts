12ca1cd0a12d504febcec5ff3501aafddfee3b18
wuyx <1007178106@qq.com>
2018-11-10 15:57:10 +0800

    实例

--- src/app/process/element-ref/element-ref.component.ts
+++ src/app/process/element-ref/element-ref.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-element-ref',
-  templateUrl: './element-ref.component.html',
-  styleUrls: ['./element-ref.component.less']
+    selector: 'app-element-ref',
+    templateUrl: './element-ref.component.html',
+    styleUrls: ['./element-ref.component.less']
 })
-export class ElementRefComponent implements OnInit {
+export class ElementRefComponent implements AfterViewInit {
 
-  constructor() { }
+    @ViewChild('buttonOne')
+    buttonOne: ElementRef;
 
-  ngOnInit() {
-  }
+    /**
+     * 我们在构造函数里面导入了ElementRef，咱们就得记住elementRef对应的nativeElement就是当前组件的顶部元素(不是DOM对象哦)
+     */
+    constructor(private elementRef: ElementRef) {
+    }
+
+    /**
+     * 初始化完组件视图及其子视图之后调用,只会调用一次
+     */
+    ngAfterViewInit() {
+        /**
+         * this.elementRef.nativeElement是当前组件的顶部元素
+         */
+        console.log(this.elementRef.nativeElement);
+        const divElement = this.elementRef.nativeElement.querySelector('div');
+        // console.log(divElement);
+        this.buttonOne.nativeElement.style.backgroundColor = 'red';
+    }
 
 }
--- src/app/process/renderer2/renderer2.component.ts
+++ src/app/process/renderer2/renderer2.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-renderer2',
-  templateUrl: './renderer2.component.html',
-  styleUrls: ['./renderer2.component.less']
+    selector: 'app-renderer2',
+    templateUrl: './renderer2.component.html',
+    styleUrls: ['./renderer2.component.less']
 })
-export class Renderer2Component implements OnInit {
+export class Renderer2Component implements AfterViewInit {
 
-  constructor() { }
+    @ViewChild('parent')
+    parentDiv: ElementRef;
 
-  ngOnInit() {
-  }
+    @ViewChild('buttonOne')
+    buttonOne: ElementRef;
+
+    constructor(private renderer2: Renderer2) {
+
+    }
+
+    /**
+     * 初始化完组件视图及其子视图之后调用,只会调用一次
+     */
+    ngAfterViewInit(): void {
+
+        // 给页面button添加样式
+        this.renderer2.setStyle(this.buttonOne.nativeElement, 'backgroundColor', 'red');
+        // 给页面button添加点击事件,点击添加一个空间到div节点
+        this.renderer2.listen(this.buttonOne.nativeElement, "click", (event) => {
+            console.log(event);
+            // 我们创建一个input节点元素，并且把他添加到div下面
+            const inputCreate: Element = this.renderer2.createElement("input");
+            inputCreate.setAttribute("type", "text");
+            inputCreate.setAttribute("name", "q");
+            inputCreate.setAttribute("value", "使用setAttribute");
+            inputCreate.setAttribute("onclick", "javascript:alert('This is a text!');");
+            this.renderer2.appendChild(this.parentDiv.nativeElement, inputCreate);
+        });
+
+        // 创建文本元素
+        const textCreate = this.renderer2.createText("我是创建的文本");
+        this.renderer2.appendChild(this.parentDiv.nativeElement, textCreate);
+
+        // 创建comment注解元素
+        const commentCreate = this.renderer2.createComment("我是创建的注解");
+        this.renderer2.appendChild(this.parentDiv.nativeElement, commentCreate);
+
+    }
 
 }
